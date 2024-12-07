0b8033b49c9ad77d26081a23e53c9bd83e73691f
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-07 17:57:10 -0400

    Outras formas de fazer...

--- ExeFor/assets/js/index.js
+++ ExeFor/assets/js/index.js
@@ -20,3 +20,15 @@ for (let i = 0; i < elementos.length; i++) {
 }
 
 
+// De outras maneiras
+
+const div = document.createElement('div');
+
+for(let i = 0; i < elementos.length; i++) {
+    let {tag, texto} = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerText = texto;
+    div.appendChild(tagCriada);
+}
+
+section.appendChild(div);
