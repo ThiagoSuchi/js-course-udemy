c8076324fe02eed8875ada48357548623e42fc4a
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-07 17:57:10 -0400

    Outras formas de fazer...

--- ExeFor/assets/js/index.js
+++ ExeFor/assets/js/index.js
@@ -8,15 +8,27 @@ const elementos = [
     {tag: 'p', texto: 'Dia de jogar um volei!'}
 ];
 
-function createElement(element, text) {
-    const elementHTML = document.createElement(element)
-    elementHTML.innerHTML = text
-    return elementHTML
+// function createElement(element, text) {
+//     const elementHTML = document.createElement(element)
+//     elementHTML.innerHTML = text
+//     return elementHTML
+// }
+
+// for (let i = 0; i < elementos.length; i++) {
+//     const {tag, texto} = elementos[i]
+//     section.appendChild(createElement(tag, texto))
+// }
+
+
+// De outras maneiras
+
+const div = document.createElement('div');
+
+for(let i = 0; i < elementos.length; i++) {
+    let {tag, texto} = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerText = texto;
+    div.appendChild(tagCriada);
 }
 
-for (let i = 0; i < elementos.length; i++) {
-    const {tag, texto} = elementos[i]
-    section.appendChild(createElement(tag, texto))
-}
-
-
+section.appendChild(div);
