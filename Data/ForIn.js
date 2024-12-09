d79dc8fb8512fdf3abb21fd02f5af34c3659e8c0
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-09 10:14:40 -0400

    For In - Estrutura de repetição

---
+++ Data/ForIn.js
@@ -0,0 +1,20 @@
+const frutas = ['Pera', 'Maçã', 'Uva'];
+
+for (let i in frutas){
+    console.log(i);// 0
+    console.log(frutas);// ['Pera', 'Maçã', 'Uva']
+    console.log(i, frutas[i]);// 0 Pera
+    console.log('-------------------------');
+};
+
+// For in - lê os índices ou chaves do objeto
+
+const pessoa = {
+    nome: 'Alberto',
+    sobrenome: 'Silva Gomes',
+    idade: 22,
+};
+
+for (let chave in pessoa){
+    console.log(chave + ':', pessoa[chave]);
+};
