bdc3084722a1c6b5414ed2b59aeba3df7146e48d
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-09 10:32:02 -0400

    For of - Estrutura de repetição

---
+++ Data/ForOf.js
@@ -0,0 +1,25 @@
+/* é uma estrutura de repetição do 
+JavaScript que serve para percorrer itens 
+de objetos iteráveis, como arrays, strings, 
+maps, sets, e outros. */
+const nome = ["João Paulo", "Laura Viera"];
+
+for (let valor of nome) {
+  // valor: Representa o valor de cada item no objeto iterável durante a iteração.
+  // nome: É o objeto que você deseja percorrer (ex.: um array ou string).
+  console.log(nome, valor);
+  break;
+}
+
+console.log('-------------------------------------------------');
+
+const usuarios = new Map([
+  [1, "João"],
+  [2, "Maria"],
+  [3, "Carlos"],
+]);
+
+for (const [id, nome] of usuarios) {
+  console.log(`ID: ${id}, Nome: ${nome}`);
+}
+
