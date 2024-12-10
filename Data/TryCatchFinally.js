c3c77d429832c367728578462b0d64bb84b647ef
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-10 18:18:47 -0400

    Tratando erro - Function data atual try/catch/finally

--- Data/TryCatchFinally.js
+++ Data/TryCatchFinally.js
@@ -14,3 +14,32 @@ try {
 ou finalizar processos, como fechar conexões,
 liberar recursos, ou exibir mensagens. 
 */
+
+console.log('--------------------------------------------------');
+
+function dataAtual(data) {
+    if(data && !(data instanceof Date)){
+        throw new Error('Não é uma data!');
+    }
+
+    if(!data) {
+        data = new Date()
+    }
+
+    return data.toLocaleDateString('pt-br', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+}
+
+try {
+    console.log(dataAtual());
+    console.log(dataAtual(new Date()));
+    console.log(dataAtual(eae));
+} catch(e) {
+    console.error(e.message);
+} finally {
+    console.log('Date');
+}
