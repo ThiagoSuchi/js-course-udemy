9a4d2118e11bb5c2e8dfc1cd6e6422d6efe333ba
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-11 17:37:08 -0400

    Criando um timer com setInterval

--- ExeTimer/assets/js/index.js
+++ ExeTimer/assets/js/index.js
@@ -5,40 +5,40 @@ const btnPausar = document.querySelector(".pausar");
 const btnZerar = document.querySelector(".zerar");
 
 function zeroEsquerda(hours) {
-  return hours > 9 ? hours : `0${hours}`;
+    return hours > 9 ? hours : `0${hours}`;
 }
 
 let sec = 0;
 let time = null;
 
 btnIniciar.addEventListener("click", () => {
-  relogio.style.color = "white";
+    relogio.classList.remove("pausado");
 
-  if (!time) {
-    time = setInterval(() => {
-      sec++;
+    if (!time) {
+        time = setInterval(() => {
+            sec++;
 
-      // Formatando hora-minuto-segundo em hh/mm/ss
-      let second = sec % 60;
-      let minute = Math.floor(sec / 60) % 60;
-      let hours = Math.floor(sec / 3600);
+            // Formatando hora-minuto-segundo em hh/mm/ss
+            let second = sec % 60;
+            let minute = Math.floor(sec / 60) % 60;
+            let hours = Math.floor(sec / 3600);
 
-      relogio.innerHTML = `${zeroEsquerda(hours)}:${zeroEsquerda(
-        minute
-      )}:${zeroEsquerda(second)}`;
-    }, 1000);
-  }
+            relogio.innerHTML = `${zeroEsquerda(hours)}:${zeroEsquerda(
+                minute
+            )}:${zeroEsquerda(second)}`;
+        }, 1000);
+    }
 });
 
 btnPausar.addEventListener("click", () => {
-    relogio.style.color = "red";
+    relogio.classList.add("pausado");
     clearInterval(time);
     time = null;
 });
 
 btnZerar.addEventListener("click", () => {
-    relogio.style.color = "white";
-    clearInterval(time)
+    relogio.classList.remove("pausado");
+    clearInterval(time);
     time = null;
     sec = 0;
     relogio.innerHTML = "00:00:00";
