718ab388ce77d028e7c392b0265edfbe4ea73b66
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-25 15:43:23 -0400

    Funções para adicionar, e deletar foram feitas...

--- ExeListTarefa/assets/js/index.js
+++ ExeListTarefa/assets/js/index.js
@@ -14,13 +14,35 @@ inputTarefa.addEventListener('keypress', function(e) {
     }
 })
 
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const btnApagar = document.createElement('button');
+    btnApagar.innerText = 'Apagar';
+    btnApagar.setAttribute('class', 'apagar');
+    li.appendChild(btnApagar);
+}
+
 function criarTarefa(textoInput) {
     const li = criarLi();
     li.innerText = textoInput;
-    tarefas.appendChild(li)
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
 }
 
 btnTarefa.addEventListener('click', function() {
     if(!inputTarefa.value) return;// Impede que adicione espaços em branco
     criarTarefa(inputTarefa.value)
+})
+
+document.addEventListener('click', function(e) {
+    const el = e.target
+    if(el.classList.contains('apagar')) {
+        el.parentElement.remove()
+    }
 })
